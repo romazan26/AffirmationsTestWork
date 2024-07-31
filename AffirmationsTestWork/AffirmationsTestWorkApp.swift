e339f3ef2a3e08fcbe47bf5e785f049211d97849
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-07-31 12:04:25 +0300

    Initial Commit

---
+++ AffirmationsTestWork/AffirmationsTestWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsTestWorkApp.swift
+//  AffirmationsTestWork
+//
+//  Created by Роман on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsTestWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
